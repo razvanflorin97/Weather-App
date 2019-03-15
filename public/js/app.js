f97b356e282146fdf47a3b5f8a2f6abe0a8abf57
Nicolae Razvan Florin <razvannicolae10@gmail.com>
2019-03-15 16:27:36 +0200

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ form.addEventListener('submit', (e) => {
   message2.textContent = '';
 
   const location = search.value;
-  fetch('http://localhost:3000/weather?address='+ location).then(response => {
+  fetch('/weather?address='+ location).then(response => {
   response.json().then(data => {
     //console.log(data);
     if (data.error) {
